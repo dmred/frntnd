4b0c3fcd5610196a9b0daf8dcf131a6f42e1608a
dmred <dmredchenko@gmail.com>
2018-01-13 03:27:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ const server = http.createServer(function (request, response) {
 
 console.log('port = ', process.env.PORT);
 
-server.listen(process.env || 3000);
+server.listen(process.env.PORT || 3000);
 console.log('sever started!');
